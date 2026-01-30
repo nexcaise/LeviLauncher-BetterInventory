c021104a1fb309b5d48113dee1f5419b7115fa96
alvinqid <github@alvinq.my.id>
2026-01-30 13:04:43 +0700

    test

---
+++ src/ItemRegistryRef.cpp
@@ -0,0 +1,9 @@
+#include "ItemRegistry.hpp"
+
+std::shared_ptr<ItemRegistry> ItemRegistryRef::_lockRegistry() const
+{
+    //Assert(!mItemRegistry.expired(), "Trying to lock a expired ItemRegistry!");
+    auto lock = mItemRegistry.lock();
+    //Assert(lock.operator bool(), "ItemRegistryRef cannot access ItemRegistry - attempt to lock weak_ptr returned null!");
+    return lock;
+}
--- src/ItemRegistryRef.hpp
+++ src/ItemRegistryRef.hpp
@@ -4,5 +4,7 @@ class ItemRegistry;
 
 class ItemRegistryRef {
 public:
+    std::weak_ptr<ItemRegistry> mItemRegistry;
+
     std::shared_ptr<ItemRegistry> _lockRegistry() const;
 };
