505be06b0b9259732a81bd929f55f8d24fee734e
alvinqid <github@alvinq.my.id>
2026-02-01 05:39:46 +0700

    test

--- src/ItemRegistryRef.hpp
+++ src/ItemRegistryRef.hpp
@@ -3,22 +3,36 @@
 #include <unordered_map>
 #include "SharedPtr.hpp"
 #include "Item.hpp"
-//#include "StringHash.hpp"
 
 class ItemRegistry;
 
 class StringHash {
 public:
     int mHash;
+    bool operator==(StringHash const& o) const { return mHash == o.mHash; }
 };
+
+namespace std {
+template<>
+struct hash<StringHash> {
+    size_t operator()(StringHash const& s) const noexcept {
+        return std::hash<int>()(s.mHash);
+    }
+};
+}
 
 class ItemRegistryRef {
 public:
-    std::unordered_map<StringHash, WeakPtr<Item>> nameToItemMap;
     std::weak_ptr<ItemRegistry> mWeakRegistry;
     std::weak_ptr<ItemRegistry> mItemRegistry;
-    
-    std::unordered_map<StringHash, WeakPtr<Item>> const& getNameToItemMap() const;
 
-    std::shared_ptr<ItemRegistry> _lockRegistry() const;
+    inline std::unordered_map<StringHash, WeakPtr<Item>> const& getNameToItemMap() const {
+        return _lockRegistry()->mNameToItemMap;
+    }
+
+    inline std::shared_ptr<ItemRegistry> _lockRegistry() const {
+        auto sp = mWeakRegistry.lock();
+        if (!sp) sp = mItemRegistry.lock();
+        return sp;
+    }
 };
