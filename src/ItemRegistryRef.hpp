47ebe7f15a849327941167f6b4692c2bd434cd23
alvinqid <github@alvinq.my.id>
2026-02-01 05:20:59 +0700

    test

--- src/ItemRegistryRef.hpp
+++ src/ItemRegistryRef.hpp
@@ -3,17 +3,31 @@
 #include <unordered_map>
 #include "SharedPtr.hpp"
 #include "Item.hpp"
-
-class HashedString;
+//#include "StringHash.hpp"
 
 class ItemRegistry;
+
+class StringHash {
+public:
+    int mHash;
+
+    StringHash(const char* str) : 
+        mHash(StringToNameId(str)) 
+    {
+    }
+
+    StringHash(const std::string& str) :
+        mHash(StringToNameId(str)) 
+    {
+    }
+};
 
 class ItemRegistryRef {
 public:
     std::weak_ptr<ItemRegistry> mWeakRegistry;
     std::weak_ptr<ItemRegistry> mItemRegistry;
     
-    std::unordered_map<HashedString, WeakPtr<Item>> const& getNameToItemMap() const;
+    std::unordered_map<StringHash, WeakPtr<Item>> const& getNameToItemMap() const;
 
     std::shared_ptr<ItemRegistry> _lockRegistry() const;
 };
