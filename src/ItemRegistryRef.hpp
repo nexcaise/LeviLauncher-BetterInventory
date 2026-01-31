8f7e25bf34af0ecab885327b840d1d05d1a1d10d
alvinqid <github@alvinq.my.id>
2026-02-01 05:27:40 +0700

    test

--- src/ItemRegistryRef.hpp
+++ src/ItemRegistryRef.hpp
@@ -17,7 +17,7 @@ public:
     std::weak_ptr<ItemRegistry> mWeakRegistry;
     std::weak_ptr<ItemRegistry> mItemRegistry;
     
-    std::unordered_map<StringHash, WeakPtr<Item>> const& getNameToItemMap() const;
+    virtual std::unordered_map<StringHash, WeakPtr<Item>> const& getNameToItemMap() const;
 
     std::shared_ptr<ItemRegistry> _lockRegistry() const;
 };
