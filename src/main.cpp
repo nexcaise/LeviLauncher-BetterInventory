b8ca1c997c22ca39cc586c7ea549b7b2ee004c07
alvinqid <github@alvinq.my.id>
2026-02-06 19:24:16 +0700

    test

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,8 @@
 #include <pl/Gloss.h>
-#include "util/Logger.hpp"
 #include "features/Hooks.hpp"
-
-Logger logger("Offhand");
 
 __attribute__((constructor))
 void Init() {
-    logger.info("Init()");
     GlossInit(true);
     RegisterHooks();
 }
