29daa04f87b09515a65ba38616d5b5af29b1b451
alvinqid <github@alvinq.my.id>
2026-02-07 15:05:22 +0700

    test

--- xmake.lua
+++ xmake.lua
@@ -32,26 +32,18 @@ add_repositories("xmake-repo https://github.com/xmake-io/xmake-repo.git")
 
 add_requires(
     "fmt 10.2.1",
-    "glm 0.9.9+8",
-    "nlohmann_json 3.11.3"
+    "glm 0.9.9+8"
 )
 
-target("preloader")
-    set_kind("static")
-    add_files("preloader-android/src/**.cpp")
-    add_headerfiles("preloader-android/src/**.h")
-    add_packages("fmt", "glm", "nlohmann_json")
-    add_includedirs("preloader-android/src", {public = true})
-
-target("OffhandMod")
+target("BetterInventory")
     set_kind("shared")
     add_files("src/**.cpp")
-    add_headerfiles("src/**.hpp")
+    add_headerfiles("src/**.hpp", "preloader/src/**.hpp")
     add_includedirs(
         "src",
-        "preloader-android/src",
+        "preloader/src",
         {public = true}
     )
-    add_deps("preloader")
-    add_packages("fmt", "glm", "nlohmann_json")
-    add_links("log")
+    add_linkdirs("preloader/lib")
+    add_packages("fmt", "glm")
+    add_links("log", "preloader")
